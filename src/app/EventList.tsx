79cf9efe3e6adf9952ff82e0dcf17629a33c83ca
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2025-07-26 15:02:27 -0600

    Add time button

--- src/app/EventList.tsx
+++ src/app/EventList.tsx
@@ -68,14 +68,40 @@ export const EventList = () => {
 				}
 
 				<div className="card card-lg border rounded-4xl border-neutral-300 w-96 mt-10" >
-					{/* <div className="absolute inset-x-0 top-0 text-slate-50 mix-blend-normal font-semibold text-xl flex items-center justify-center ">
-						<div className='flex items-center gap-2 mt-4'>
-							<svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" />
-							</svg>
-							<span className='text-sm font-semibold'>31/07 10:00PM</span>
+					<div className='relative'>
+						<div className="absolute top-3 left-3">
+							<button className='btn btn-soft btn-sm rounded-tl-3xl'>
+								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z" />
+								</svg>
+								Escarola
+							</button>
 						</div>
-					</div> */}
+						<div className="dropdown dropdown-center absolute top-3 right-3">
+							<button tabIndex={0} className='btn btn-soft btn-sm rounded-tr-3xl '>
+								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+								</svg>
+								31/07, 9:00pm
+							</button>
+							<ul tabIndex={0} className="dropdown-content mt-1 menu bg-base-100 rounded-box z-1 p-2 shadow-sm w-36">
+								<div className='text-xs py-2 text-center'>
+									Agregar al calendario
+								</div>
+								<li><a className='flex items-center gap-2'>
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M120,128a32,32,0,1,0-32,32A32,32,0,0,0,120,128Zm-48,0a16,16,0,1,1,16,16A16,16,0,0,1,72,128Zm152-24H208V40a8,8,0,0,0-8-8H104a8,8,0,0,0-8,8V64H40A16,16,0,0,0,24,80v96a16,16,0,0,0,16,16H72v16a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V112A8,8,0,0,0,224,104Zm-58.34,60L216,127.65v72.7ZM112,48h80v77.24l-40,28.89V80a16,16,0,0,0-16-16H112ZM40,80h96v77.9c0,.12,0,.24,0,.36V176H40ZM88,192h48a16,16,0,0,0,16-16v-2.13L199.26,208H88Z"></path></svg>
+									Outlook
+								</a></li>
+								<li><a className='flex items-center gap-2'>
+									<svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+										<path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
+									</svg>
+									Google
+								</a></li>
+							</ul>
+						</div>
+					</div>
 					<div className='px-1 pt-1 rounded-4xl'>
 						<img
 							src={cards[0].image}
@@ -84,9 +110,9 @@ export const EventList = () => {
 						/>
 					</div>
 					<div className='card-body'>
-						<div className='justify-between flex items-center gap-8'>
-							<div className='card-title font-neuton italic text-3xl text-neutral-800'>Título del Evento</div>
-							<div className="badge badge-soft badge-lg bg-pink-100 text-pink-600 badge-outline border-0 font-semibold text-md gap-1">
+						<div className='justify-between flex items-center gap-1'>
+							<div className='card-title font-neuton italic text-3xl text-neutral-800'>Noche de Pop Rock</div>
+							<div className="badge badge-soft badge-lg bg-pink-100 text-pink-600 badge-outline border-0 font-semibold text-md gap-1 rounded-lg">
 								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 24 24">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2" />
 								</svg>
@@ -96,14 +122,14 @@ export const EventList = () => {
 						</div>
 						<p className='text-neutral-500 text-md'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis magnam est maxime.</p>
 					</div>
-					<div className='divider'></div>
+					{/* <div className='divider'></div>
 					<div className='flex '>
 						<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H184V24a8,8,0,0,0-16,0v8H88V24a8,8,0,0,0-16,0v8H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM72,48v8a8,8,0,0,0,16,0V48h80v8a8,8,0,0,0,16,0V48h24V80H48V48ZM208,208H48V96H208V208Z"></path></svg>
 						<span className='text-neutral-500 text-sm font-semibold'>31/07 10:00PM</span>
-					</div>
+					</div> */}
 				</div>
 
 			</div>
-		</div>
+		</div >
 	)
 }
