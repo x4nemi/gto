da966097bfa8a0f1918e1afe3f54c0d12c1412ee
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2025-07-26 15:11:56 -0600

    Update EventList component styles and add new button for additional options

--- src/app/EventList.tsx
+++ src/app/EventList.tsx
@@ -67,10 +67,10 @@ export const EventList = () => {
 					// ))
 				}
 
-				<div className="card card-lg border rounded-4xl border-neutral-300 w-96 mt-10" >
+				<div className="card card-lg border rounded-4xl border-neutral-300 w-96 mt-2" >
 					<div className='relative'>
 						<div className="absolute top-3 left-3">
-							<button className='btn btn-soft btn-sm rounded-tl-3xl'>
+							<button className='btn btn-soft btn-sm rounded-tl-3xl text-neutral-800'>
 								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z" />
@@ -79,7 +79,7 @@ export const EventList = () => {
 							</button>
 						</div>
 						<div className="dropdown dropdown-center absolute top-3 right-3">
-							<button tabIndex={0} className='btn btn-soft btn-sm rounded-tr-3xl '>
+							<button tabIndex={0} className='btn btn-soft btn-sm rounded-tr-3xl text-neutral-800'>
 								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 								</svg>
@@ -98,6 +98,12 @@ export const EventList = () => {
 										<path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
 									</svg>
 									Google
+								</a></li>
+								<li><a className='flex items-center gap-2'>
+									<svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+									</svg>
+									Otro
 								</a></li>
 							</ul>
 						</div>
