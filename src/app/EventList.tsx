23daf07e5e6a181b2c0cbd40f46d49bbc7d6a5eb
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2025-07-27 16:00:03 -0600

    Add profile modal of the site

--- src/app/EventList.tsx
+++ src/app/EventList.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 
 export const EventList = () => {
 	const cards = [
@@ -17,6 +17,8 @@ export const EventList = () => {
 
 		}
 	]
+
+	const [openSiteModal, setOpenSiteModal] = useState(true)
 	return (
 		<div className="card bg-base-100 card-lg shadow-sm">
 			<div className="card-body">
@@ -69,8 +71,8 @@ export const EventList = () => {
 
 				<div className="card card-lg border rounded-4xl border-neutral-300 w-96 mt-2" >
 					<div className='relative'>
-						<div className="absolute top-3 left-3">
-							<button className='btn btn-soft btn-sm rounded-tl-3xl text-neutral-800'>
+						<div className="absolute top-3 left-3 z-100">
+							<button className='btn btn-soft btn-sm rounded-tl-3xl text-neutral-800' onClick={() => setOpenSiteModal(true)}>
 								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z" />
@@ -78,7 +80,7 @@ export const EventList = () => {
 								Escarola
 							</button>
 						</div>
-						<div className="dropdown dropdown-center absolute top-3 right-3">
+						<div className="dropdown dropdown-center absolute top-3 right-3 z-100">
 							<button tabIndex={0} className='btn btn-soft btn-sm rounded-tr-3xl text-neutral-800'>
 								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -108,15 +110,15 @@ export const EventList = () => {
 							</ul>
 						</div>
 					</div>
-					<div className='px-1 pt-1 rounded-4xl'>
+					<div className='px-1 pt-1 rounded-4xl drop-shadow-2xl'>
 						<img
 							src={cards[0].image}
 							alt="Event"
 							className="rounded-4xl object-cover"
 						/>
 					</div>
-					<div className='card-body'>
-						<div className='justify-between flex items-center gap-1'>
+					<div className='card-body pt-3'>
+						<div className='justify-between flex items-center gap-0'>
 							<div className='card-title font-neuton italic text-3xl text-neutral-800'>Noche de Pop Rock</div>
 							<div className="badge badge-soft badge-lg bg-pink-100 text-pink-600 badge-outline border-0 font-semibold text-md gap-1 rounded-lg">
 								<svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 24 24">
@@ -128,13 +130,44 @@ export const EventList = () => {
 						</div>
 						<p className='text-neutral-500 text-md'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis magnam est maxime.</p>
 					</div>
-					{/* <div className='divider'></div>
-					<div className='flex '>
-						<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H184V24a8,8,0,0,0-16,0v8H88V24a8,8,0,0,0-16,0v8H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM72,48v8a8,8,0,0,0,16,0V48h80v8a8,8,0,0,0,16,0V48h24V80H48V48ZM208,208H48V96H208V208Z"></path></svg>
-						<span className='text-neutral-500 text-sm font-semibold'>31/07 10:00PM</span>
-					</div> */}
 				</div>
+				<dialog className="modal" open={openSiteModal}>
+					<div className="modal-box lg:w-96 xl:w-96 p-0">
+						<div className="flex flex-col items-center pb-10 pt-12">
+							<img className="w-24 h-24 mb-3 rounded-full shadow-lg z-100" src="/escarola_logo.jpg" alt="Escarola" />
+							<h5 className="mb-1 text-xl font-semibold text-gray-900">Escarola</h5>
+							<span className="text-sm text-gray-500">Restaurante</span>
+							<div className="inline-flex gap-1 mt-5" role="group">
+								<button className="btn btn-soft delay-150 transition shadow-xl hover:-rotate-5 hover:-translate-y-1 hover:scale-105 ease-in-out">
+									<svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a6 6 0 1 0 0-12 6 6 0 0 0 0 12Zm0 0v6M9.5 9A2.5 2.5 0 0 1 12 6.5" />
+									</svg>
+								</button>
+								<button className="btn btn-soft delay-150 transition shadow-xl hover:-rotate-2 hover:-translate-y-1 hover:scale-105 ease-in-out">
+									<svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+										<path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd" />
+									</svg>
 
+								</button>
+								<button className="btn btn-soft delay-150 transition shadow-xl hover:rotate-2 hover:-translate-y-1 hover:scale-105 ease-in-out"><svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+									<path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd" />
+								</svg>
+								</button>
+								<button className="btn btn-soft delay-150 transition shadow-xl hover:rotate-5 hover:-translate-y-1 hover:scale-105 ease-in-out">
+									<svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+										<path fill="currentColor" fill-rule="evenodd" d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z" clip-rule="evenodd" />
+										<path fill="currentColor" d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z" />
+									</svg>
+								</button>
+							</div>
+						</div>
+						<div className='absolute inset-x-0 top-0 lg:w-96 xl:w-96 min-md:w-full h-32 bg-linear-to-r from-amber-100 via-rose-200 to-sky-100 rounded-2xl z-0'></div>
+						<form method="dialog">
+							{/* if there is a button in form, it will close the modal */}
+							<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={() => setOpenSiteModal(false)}>✕</button>
+						</form>
+					</div>
+				</dialog>
 			</div>
 		</div >
 	)
